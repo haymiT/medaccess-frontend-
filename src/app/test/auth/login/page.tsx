5e82ee4f09e933c27d5a2ef56bf0f3436ae47961
Dagwal <dagimwalelegne@gmail.com>
2024-10-12 21:56:21 +0300

    auth component init

---
+++ src/app/test/auth/login/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+// LoginForm.js
+import React from 'react';
+import '../../../components/static/styles/login.css'
+
+const LoginForm = () => {
+    const validateForm = () => {
+        // Add validation logic here
+        return true;
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form action="/login" method="POST" onSubmit={validateForm}>
+                <input type="text" id="username" name="username" placeholder="Username" />
+                <input type="password" id="password" name="password" placeholder="Password" />
+                <input type="submit" value="Log In" />
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
---
+++ src/app/test/auth/logout/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+// LogoutPage.js
+import React from 'react';
+
+const LogoutPage = () => {
+    return (
+        <div>
+            <h2>Welcome User</h2>
+            <button id="logout-btn" onClick={() => console.log('Logged out')}>Logout</button>
+        </div>
+    );
+};
+
+export default LogoutPage;
