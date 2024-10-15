2da17662c9652577596cfa245d6551534bc0109a
Dagwal <dagimwalelegne@gmail.com>
2024-10-16 00:41:27 +0300

    fix: build error

--- src/app/(dashboard)/rbac/role-management/[id]/edit/layout.tsx
+++ src/app/(dashboard)/rbac/role-management/[id]/edit/layout.tsx
@@ -1,26 +1,12 @@
 "use client";
-import { useMediaQuery } from "@mantine/hooks";
-import Sidebar from "@/app/_components/sidebar/sidebar";
-import { Box, Flex, Text } from "@mantine/core";
-import styles from "../../../_components/sidebar/sidebar.module.scss";
+import { Box } from "@mantine/core";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
-    <Flex>
-      <nav
-        className={styles.nav}
-        style={{
-          display: isMobile ? "none" : "block",
-        }}
-      >
-        <Sidebar />
-      </nav>
-      <Box className="w-full flex p-6  bg-[#e7f4f7]">
-        <Box className=" w-full p-6 bg-white">
-          {children}
-        </Box>
+    <Box className="w-full flex p-6  bg-[#e7f4f7]">
+      <Box className=" w-full p-6 bg-white">
+        {children}
       </Box>
-    </Flex>
+    </Box>
   );
 }
